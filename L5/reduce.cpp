62b50b59eabca68a925aa208aa818cd17469c2c2
ginloy <liyongleow@gmail.com>
2025-03-30 23:12:49 +0800

    L4 and L5 code

---
+++ L5/reduce.cpp
@@ -0,0 +1,33 @@
+/**
+ * CS3210 - Collective communication in MPI.
+ */
+
+#include <mpi.h>
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+	int numtasks, rank, sendcount, recvcount, source;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+	source = 0;
+	sendcount = 1;
+	recvcount = 1;
+
+	// Generate a random value for each process
+	srand(rank);
+	int localval = rand() % 10;
+	printf("Rank %d generated value %d\n", rank, localval);
+
+	// TODO: Exercise 4
+
+	MPI_Finalize();
+
+	return 0;
+
+}
+
--- shell.nix
+++ shell.nix
@@ -1,7 +1,15 @@
-{ pkgs ? import <unstable> {} }:
-pkgs.mkShell {
+{pkgs ? import <nixpkgs> {}}:
+(pkgs.mkShell.override {stdenv = pkgs.gcc13Stdenv;}) {
   nativeBuildInputs = with pkgs; [
     texliveFull
     texlab
   ];
+  buildInputs = with pkgs; [
+    cudaPackages.cudatoolkit
+    cudaPackages.cuda_cudart
+    mpi
+  ];
+  CUDA_PATH = "${pkgs.cudaPackages.cudatoolkit}";
+  EXTRA_LDFLAGS = "-L/lib";
+  EXTRA_CCFLAGS = "-I/usr/include";
 }
