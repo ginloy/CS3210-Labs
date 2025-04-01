c6838d7e095900ba08decdcd3df8e94c5fb1d077
Leow Li Yong <e0725404@soctf-pdc-033.d1.comp.nus.edu.sg>
2025-04-01 15:17:22 +0800

    L5 stuff

--- L5/col_comm.cpp
+++ L5/col_comm.cpp
@@ -2,45 +2,53 @@
  * CS3210 - Collective communication in MPI.
  */
 
-#include <mpi.h>
 #include <cstdio>
+#include <mpi.h>
 #define SIZE 4
 
-int main(int argc, char *argv[])
-{
-	int numtasks, rank, sendcount, recvcount, source;
-	float sendbuf[SIZE][SIZE] = 
-	{
-		{1.0, 2.0, 3.0, 4.0},
-		{5.0, 6.0, 7.0, 8.0},
-		{9.0, 10.0, 11.0, 12.0},
-		{13.0, 14.0, 15.0, 16.0}  
-	};
-	float recvbuf[SIZE];
+int main(int argc, char *argv[]) {
+  int numtasks, rank, sendcount, recvcount, source;
+  float sendbuf[SIZE][SIZE] = {{1.0, 2.0, 3.0, 4.0},
+                               {5.0, 6.0, 7.0, 8.0},
+                               {9.0, 10.0, 11.0, 12.0},
+                               {13.0, 14.0, 15.0, 16.0}};
+  float recvbuf[SIZE];
 
-	MPI_Init(&argc,&argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-	if (numtasks == SIZE) {
-		source = 0;
-		sendcount = SIZE;
-		recvcount = SIZE;
-  	
-		MPI_Scatter(sendbuf,sendcount,MPI_FLOAT,recvbuf,recvcount,
-			MPI_FLOAT,source,MPI_COMM_WORLD);
-		printf("rank= %d  Results: %f %f %f %f\n",rank,recvbuf[0],
-			recvbuf[1],recvbuf[2],recvbuf[3]);
+  if (numtasks == SIZE) {
+    source = 0;
+    sendcount = SIZE;
+    recvcount = SIZE;
 
-		// TODO: Exercise 3
-		
-	} else {
-		printf("Must specify %d processes. Terminating.\n",SIZE);
-	}
+    MPI_Scatter(sendbuf, sendcount, MPI_FLOAT, recvbuf, recvcount, MPI_FLOAT,
+                source, MPI_COMM_WORLD);
+    printf("rank= %d  Results: %f %f %f %f\n", rank, recvbuf[0], recvbuf[1],
+           recvbuf[2], recvbuf[3]);
 
-	MPI_Finalize();
-	
-	return 0;
-	
+    // TODO: Exercise 3
+    float sum = 0.0;
+    for (int i = 0; i < 4; ++i) {
+      sum += recvbuf[i];
+    }
+
+    MPI_Gather(&sum, 1, MPI_FLOAT, recvbuf, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+      sum = 0.0;
+      for (int i = 0; i < 4; ++i) {
+        sum += recvbuf[i];
+      }
+      printf("Total Sum: %f\n", sum);
+    }
+
+  } else {
+    printf("Must specify %d processes. Terminating.\n", SIZE);
+  }
+
+  MPI_Finalize();
+
+  return 0;
 }
-
--- L5/reduce.cpp
+++ L5/reduce.cpp
@@ -2,32 +2,32 @@
  * CS3210 - Collective communication in MPI.
  */
 
-#include <mpi.h>
 #include <cstdio>
 #include <cstdlib>
+#include <mpi.h>
 
-int main(int argc, char *argv[])
-{
-	int numtasks, rank, sendcount, recvcount, source;
+int main(int argc, char *argv[]) {
+  int numtasks, rank, sendcount, recvcount, source;
 
-	MPI_Init(&argc,&argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-	source = 0;
-	sendcount = 1;
-	recvcount = 1;
+  source = 0;
+  sendcount = 1;
+  recvcount = 1;
 
-	// Generate a random value for each process
-	srand(rank);
-	int localval = rand() % 10;
-	printf("Rank %d generated value %d\n", rank, localval);
+  // Generate a random value for each process
+  srand(rank);
+  int localval = rand() % 10;
+  printf("Rank %d generated value %d\n", rank, localval);
 
-	// TODO: Exercise 4
+  int sum = 0;
+  // TODO: Exercise 4
+  MPI_Allreduce(&localval, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  printf("Rank %d, Total: %d\n", rank, sum);
 
-	MPI_Finalize();
+  MPI_Finalize();
 
-	return 0;
-
+  return 0;
 }
-
