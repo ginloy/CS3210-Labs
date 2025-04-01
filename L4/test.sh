b12e10eef06d69ee217fd626e262d357638579b0
Leow Li Yong <e0725404@soctf-pdc-033.d1.comp.nus.edu.sg>
2025-04-01 11:30:46 +0800

    Modify test script and obtain test results

--- L4/test.sh
+++ L4/test.sh
@@ -3,7 +3,7 @@
 TMP_DIR=$(mktemp -d)
 trap 'rm -rf "$TMP_DIR"' EXIT
 
-EXE="$TMP_DIR/mm"
+EXE="$(pwd)/mm"
 STDERR_PIPE="$TMP_DIR/stderr"
 STDOUT_PIPE="$TMP_DIR/stdin"
 mkfifo "$STDERR_PIPE" "$STDOUT_PIPE"
@@ -18,18 +18,27 @@ format_output() {
   local stdout
   local stderr
   local workers
+  local tmp_dir
+  local stdout_pipe
+  local stderr_pipe
+  tmp_dir=$(mktemp -d)
+  trap 'rm -rf "$tmp_dir"' RETURN
+  stdout_pipe="$tmp_dir/stdout"
+  stderr_pipe="$tmp_dir/stderr"
+  mkfifo "$stdout_pipe" "$stderr_pipe"
   for run in $(seq 1 5); do
-    eval "$1" >"$STDOUT_PIPE" 2>"$STDERR_PIPE" &
-    exec 3<"$STDOUT_PIPE" 4<"$STDERR_PIPE"
+    eval "$1" >"$stdout_pipe" 2>"$stderr_pipe" &
+    exec 3<"$stdout_pipe" 4<"$stderr_pipe"
     stdout=$(cat <&3)
     stderr=$(cat <&4)
     exec 3<&- 4<&-
     if [[ $stdout ]]; then
       >&2 echo "$stdout"
     fi
+    >&2 echo "$stderr"
     total_time=$(sed -E -n 's/^.*total time= +([0-9]+[.][0-9]+).*$/\1/p' <<<"$stderr")
     workers=$(sed -E -n \
-      's/^.*WORKER +([0-9]+).*communication_time= +([0-9]+[.][0-9]+).*computation_time= +([0-9]+[.][0-9]+).*$/\1,\2,\3/p' \
+      's/^.*WORKER +([0-9]+) +\(on (.*)\): +communication_time= +([0-9]+[.][0-9]+).*computation_time= +([0-9]+[.][0-9]+).*$/\1,\2,\3,\4/p' \
       <<<"$stderr")
     for row in $workers; do
       echo "$2,$3,$run,$row,$total_time"
@@ -37,9 +46,11 @@ format_output() {
   done
 }
 
-echo 'test_type,partition,run,worker,communication_time,computation_time,total_time'
-format_output "srun -p i7-7700 -N 1 -n 5 --cpu-bind=thread $EXE" A i7-7700
-format_output "srun -p xs-4114 -N 1 -n 5 --cpu-bind=thread $EXE" A xs-4114
-format_output "srun --constraint='[i7-7700*1&xs-4114*1]' --distribution=cyclic -N 2 -n 5 --cpu-bind=thread $EXE" A both
-format_output "srun -n5 -N1 -p i7-7700 $EXE 256" B i7-7700
-format_output "srun -n9 -N1 --overcommit -p i7-7700 $EXE 256" B i7-7700
+echo 'test_type,partition,run,worker,node,communication_time,computation_time,total_time'
+format_output "srun -p i7-7700 -N 1 -n 5 --cpu-bind=thread $EXE" A i7-7700 &
+format_output "srun -p xs-4114 -N 1 -n 5 --cpu-bind=thread $EXE" A xs-4114 &
+format_output "srun --constraint='[i7-7700*1&xs-4114*1]' --distribution=cyclic -N 2 -n 5 --cpu-bind=thread $EXE" A both &
+format_output "srun -n5 -N1 -p i7-7700 $EXE 256" B i7-7700 &
+format_output "srun -n9 -N1 --overcommit -p i7-7700 $EXE 256" B i7-7700 &
+
+wait
